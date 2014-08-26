8ed36762a6610b6921cd99494c7a65263cae177d
Randolph Ives <randolphives@Randolphs-MacBook-Pro.local>
2014-08-26 11:03:35 -0400

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable
+  has_many :tasks
+  def admin?
+    admin
+  end
+end
